de23af4e28005e33f6bb78e679b7c8cbdc1fefdd
Purushottam <pallipurushotham333@gmail.com>
2025-02-09 22:22:28 +0530

    added use and video models

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const connnectDB = async () => {
+const connectDB = async () => {
     try{
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`\n MongoDB connected! DB Host: ${connectionInstance.connection.host}`);  
@@ -12,4 +12,4 @@ const connnectDB = async () => {
     }
 }
 
-export default connnectDB
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import dotenv from 'dotenv'
 
 
-import connnectDB from './db/index.js'
-dotenv.config({
-    path: './env'
-})
+import connectDB from './db/index.js'
+dotenv.config()
+import express from 'express'
+const app = express()
+const port = process.env.PORT || 5000
     
 
-connnectDB()
+connectDB()
+.then(() => {
+    app.listen(port , () => {
+        console.log(`server is running at port : ${port}`)
+    });
+    app.on("error" , (error) => {
+        console.log("error : " , error);
+    })
+    
+})
+.catch((err) => {
+    console.log("mongoDB connection failed" , err)
+})
